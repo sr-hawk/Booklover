3705e14db38bb5f1f571a12e2e5780eeede66d0e
sr-hawk <157434187+sr-hawk@users.noreply.github.com>
2024-03-31 17:43:58 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Booklover',
+      version='1',
+      description='Functions to track book reading and ratings',
+      url='https://github.com/sr-hawk/Booklover',
+      author='Alec Pixton',
+      author_email='etk3pu@virginia.com',
+      license='MIT',
+      packages=['Booklover'])
